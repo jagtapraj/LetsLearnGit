d0c0d9c0ba5add48a8f8fd8bf007b527b06aefab
Raj Jagdish Jagtap <rajagtap2002@gmail.com>
2021-07-11 20:01:05 +0530

    second commit

---
+++ got.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int main()
+{
+    int a,b,c;
+    a=1;
+    b=2;
+    c=a*b;
+    cout<<c;
+}
