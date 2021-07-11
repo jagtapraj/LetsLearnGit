22d038568076d88880854a518e06f5b048bde908
Raj Jagdish Jagtap <rajagtap2002@gmail.com>
2021-07-11 19:47:54 +0530

    initial commit

---
+++ basic.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int main()
+{
+    int a=0,b=0;
+    int c;
+    c=a+b;
+    cout<<c;
+}
